2b64254627d4a11a4819e7a50a1608a9e8c947ab
Owen Dobson <Otdobson@outlook.com>
2024-04-26 22:43:34 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,20 +2,20 @@ export default function Navbar() {
   return (
     <header>
       <a href="/">
-        <h1>OWEN DOBSON PHOTOGRAPHY && MODELING</h1>
+        <h1>Dobson,Owen</h1>
       </a>
       <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/bloglist">Blog</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="https://linkedin.com/in/owendob23">Linkedin</a></li>
-          <li><a href="/notebook/">Notebook</a></li>
-          <li><a href="/journal/">Journal</a></li>
-          <li><a href="/resources/">Resources</a></li>
-          <li><a href="/faq/">FAQ</a></li>
-          <li><a href="/contact/">Contact</a></li>
-        </ul>
+        <p>
+          <a href="/">Home</a>
+          <a href="/bloglist">Blog</a>
+          <a href="/contact">Contact</a>
+          <a href="/resources">Resources</a>
+          <a href="/partnerships">Partnerships</a>
+          <a href="/portfolio">Portfolio</a>
+          <a href="/privacy_policy">Privacy Policy</a>
+          <a href="/tos">Terms of Service</a>
+          <a href="/FAQ">FAQ</a>
+        </p>
       </nav>
     </header>
   );
